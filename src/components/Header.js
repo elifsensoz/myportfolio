cc8e65cb8fe3fa189136af8a6305e9d8f254927c
elifsensoz <elfsensoz@gmail.com>
2023-01-20 05:36:10 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <img class="masthead-avatar mb-5" src="work.jpg" alt="..." />
+                <h1 class="masthead-heading text-uppercase mb-0">ELIF CICEK SENSOZ</h1>
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <p class="masthead-subheading font-weight-light mb-0">MERN Stack Developer</p>
+            </div>
+        </header>
+  )
+}
